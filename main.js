98885a2055ed66c1df9a2a8131a2c5bbcd2673de
hanhnhitran <hanhnhi0705@gmail.com>
2022-02-28 23:31:43 +0700

    js

---
+++ main.js
@@ -0,0 +1,52 @@
+let renderData = async (music) => {
+    let drawData = await fetch(
+      `https://api.apify.com/v2/key-value-stores/EJ3Ppyr2t73Ifit64/records/LATEST`
+    );
+    let data = await drawData.json();
+    let album = data.songs;
+  
+    let topVN = document.querySelector("#top-VN");
+    let topUSUK = document.querySelector("#top-usuk");
+    let topAs = document.querySelector("#top-asia")
+  
+    topVN.innerHTML = "";
+    topUSUK.innerHTML = "";
+    topAs.innerHTML = "";
+    for (let i = 0; i < 5; i++) {
+      let html = `<li class="splide__slide">
+                                  
+          <div class="large-boxes_box">
+              <img src="${album.top100_VN[0].songs[i].avatar}" alt="">
+              <div class="large-boxes_box--title">${album.top100_VN[0].songs[i].title}</div>
+          </div>
+          
+      </li>`;
+  
+      topVN.innerHTML += html;
+    }
+  
+    for (let i = 0; i < 5; i++) {
+      let html = `<li class="splide__slide">
+                                          
+                  <div class="large-boxes_box">
+                      <img src="${album.top100_AM[0].songs[i].avatar}" alt="">
+                      <div class="large-boxes_box--title">${album.top100_AM[0].songs[i].title}</div>
+                  </div>
+                  
+              </li>`;
+      topUSUK.innerHTML += html;
+    }
+    for (let i = 0; i < 5; i++) {
+      let html = `<li class="splide__slide">
+                                          
+                  <div class="large-boxes_box">
+                      <img src="${album.top100_CA[0].songs[i].avatar}" alt="">
+                      <div class="large-boxes_box--title">${album.top100_CA[0].songs[i].title}</div>
+                  </div>
+                  
+              </li>`;
+      topAs.innerHTML += html;
+    }
+  };
+  
+  renderData();
